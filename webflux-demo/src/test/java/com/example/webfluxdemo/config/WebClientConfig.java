e7ed62a92f100d701387fcf0e6408503a12055fc
pedrocampos <pedro.campos@startaideia.com.br>
2022-09-09 18:36:08 -0300

    feat: setting auth token

--- webflux-demo/src/test/java/com/example/webfluxdemo/config/WebClientConfig.java
+++ webflux-demo/src/test/java/com/example/webfluxdemo/config/WebClientConfig.java
@@ -2,7 +2,11 @@ package com.example.webfluxdemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class WebClientConfig {
@@ -12,6 +16,14 @@ public class WebClientConfig {
         return WebClient
                 .builder()
                 .baseUrl("http://localhost:8080")
+                .filter(this::sessionToken)
                 .build();
+    }
+
+    private Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction ex) {
+        System.out.println("generating session token");
+        ClientRequest clientRequest =
+                ClientRequest.from(request).headers(h -> h.setBearerAuth("some-lengthy-jwt")).build();
+        return ex.exchange(clientRequest);
     }
 }
